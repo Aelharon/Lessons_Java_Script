9e7bd13b21ad55ad190b9d862f18b282d8261d83
Aelharon <Aelharon@gmail.com>
2025-07-31 13:21:49 +0300

    add lesson_2

--- lesson_2/js/task0.js
+++ lesson_2/js/task0.js
@@ -11,7 +11,7 @@ const c = parseFloat('12')
 const S1 = a + c + b
 document.write(`<p> Сума S1 = ${S1} </p>`)
 
-const S2 = Math.sqrt((a + b) / 2 * a)
+const S2 = Math.sqrt((a + b) / (2 * a))
 document.write(`<p> Корінь S2 = ${S2} </p>`)
 
 const S3 = Math.cbrt((a + b) * c)
