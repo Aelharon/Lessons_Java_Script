4acf4631f77ced201c749c50bcb55c510e0a13c3
Aelharon <Aelharon@gmail.com>
2025-08-13 12:35:37 +0300

    update lesson 7

--- lesson_7/js/task_practice_1.js
+++ lesson_7/js/task_practice_1.js
@@ -15,4 +15,37 @@
 
 
 
+function getRes(num1, num2, num3) {
+	let even = 0
+	let positive = 0
+	let num100 = 0
+	if (num1 % 2 === 0) ++even
+	if (num2 % 2 === 0) ++even
+	if (num3 % 2 === 0) ++even
+	if (num1 > 0) ++positive
+	if (num2 > 0) ++positive
+	if (num3 > 0) ++positive
+	if (num1 > 100) ++num100
+	if (num2 > 100) ++num100
+	if (num3 > 100) ++num100
+	return `Кількість парних = ${even} <br> 
+			Кількість додатних = ${positive} <br> 
+			Кількість більших за 100 = ${num100}`
+}
+
+const number1 = parseInt(prompt('Введіть перше число', '102'))
+const number2 = parseInt(prompt('Введіть друге число', '61'))
+const number3 = parseInt(prompt('Введіть третє число', '-20'))
+
+getRes(number1, number2, number3)
+
+document.write(`
+	Перше число = ${number1} <br>
+	Друге число = ${number2} <br>
+	Третє число = ${number3} <br>
+	<br>
+	${getRes(number1, number2, number3)} <br>
+`)
+
+
 // }
