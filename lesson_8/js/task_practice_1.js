f13e44d770514a21a5b8383438a09b370522195b
Aelharon <Aelharon@gmail.com>
2025-08-17 14:03:53 +0300

    add lesson 8

--- lesson_8/js/task_practice_1.js
+++ lesson_8/js/task_practice_1.js
@@ -6,46 +6,64 @@
 // Крок 3. виведення результату
 
 
-// Задача №4
-// Створити функцію, яка для 3 заданих чисел знаходить
-// одночасно декілька результатів: кількість парних,
-// кількість додатних, кількість більших за 100.
+// Задача №7
+// Дано послідовність платіжок протягом року.
+// Знайти сумарну кількість грошей за:
+// 1)	за весь рік;
+// 2)	у першій половині року;
+// 3)	у другій половині року;
+// 4)	за літо;
+// 5)	за ІІ квартал;
+// 6)	за парні місяці (з парними номерами);
+// 7)	за місяці, які є початковими у сезоні (весна, літо, осінь, зима).
+
 
 if (confirm('Почати тестування?')) {
 
+	function getSumBills(...numbers) {
+		let sumAllYear = 0
+		for (let i = 0; i < numbers.length; i++) {
+			sumAllYear += numbers[i]
+		}
+		let sumFirstHalfYear = 0
+		for (let half = 0; half < 6; half++) {
+			sumFirstHalfYear += numbers[half]
+		}
+		let sumEndHalfYear = 0
+		for (let half2 = 6; half2 < numbers.length; half2++) {
+			sumEndHalfYear += numbers[half2]
+		}
+		let sumSummer = 0
+		for (let j = 5; j < 8; j++) {
+			sumSummer += numbers[j]
+		}
+		let sumSecondQuarter = 0
+		for (let q = 3; q < 6; q++) {
+			sumSecondQuarter += numbers[q]
+		}
+		let sumPairNumberMonth = 0
+		for (let p = 1; p < numbers.length; p += 2) {
+			sumPairNumberMonth += numbers[p]
+		}
+		let sumFirstMonthSeason = 0
+		for (let f = 2; f < numbers.length; f += 3) {
+			sumFirstMonthSeason += numbers[f]
+		}
 
 
-	function getRes(num1, num2, num3) {
-		let even = 0
-		let positive = 0
-		let num100 = 0
-		if (num1 % 2 === 0) ++even
-		if (num2 % 2 === 0) ++even
-		if (num3 % 2 === 0) ++even
-		if (num1 > 0) ++positive
-		if (num2 > 0) ++positive
-		if (num3 > 0) ++positive
-		if (num1 > 100) ++num100
-		if (num2 > 100) ++num100
-		if (num3 > 100) ++num100
-		return `Кількість парних = ${even} <br> 
-			Кількість додатних = ${positive} <br> 
-			Кількість більших за 100 = ${num100}`
+		return `Сумарна кількість грошей за рік - ${sumAllYear} <br>
+	Сумарна кількість грошей за першу половину року - ${sumFirstHalfYear} <br>
+	Сумарна кількість грошей за другій половину року - ${sumEndHalfYear} <br>
+	Сумарна кількість грошей за літо - ${sumSummer} <br>
+	Сумарна кількість грошей ІІ квартал - ${sumSecondQuarter} <br>
+	Сумарна кількість за парні місяці - ${sumPairNumberMonth} <br>
+	Сумарна кількість за місяці, які є початковими у сезоні - ${sumFirstMonthSeason} <br>
+	`
 	}
 
-	const number1 = parseInt(prompt('Введіть перше число', '102'))
-	const number2 = parseInt(prompt('Введіть друге число', '61'))
-	const number3 = parseInt(prompt('Введіть третє число', '-20'))
 
-	getRes(number1, number2, number3)
-
-	document.write(`
-	Перше число = ${number1} <br>
-	Друге число = ${number2} <br>
-	Третє число = ${number3} <br>
-	<br>
-	${getRes(number1, number2, number3)} <br>
-`)
+	let bills = getSumBills(650, 950, 1100, 3500, 1800, 4100, 4200, 3700, 3300, 2900, 3450, 3250)
+	document.write(bills)
 
 
 }
