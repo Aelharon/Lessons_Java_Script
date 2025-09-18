619955ce90046250bf3a61bc3fd5d9736b895f55
Aelharon <Aelharon@gmail.com>
2025-09-18 18:02:42 +0300

    add lesson 17

---
+++ lesson_17/js/task_2.js
@@ -0,0 +1,26 @@
+"use strict";
+if (confirm('Почати тестування?')) {
+    class ServiceCar {
+        constructor(driver, brand, autoNumber) {
+            if (ServiceCar.driverRef)
+                return ServiceCar.driverRef;
+            if (ServiceCar.brandRef)
+                return ServiceCar.brandRef;
+            if (ServiceCar.autoNumberRef)
+                return ServiceCar.autoNumberRef;
+            ServiceCar.driverRef = driver;
+            ServiceCar.brandRef = brand;
+            ServiceCar.autoNumberRef = autoNumber;
+        }
+        toString() {
+            return `Водій - ${ServiceCar.driverRef} <br>
+				Марка авто - ${ServiceCar.brandRef} <br>
+				Номер авто - ${ServiceCar.autoNumberRef}<hr>`;
+        }
+    }
+    const car1 = new ServiceCar('Ken Block', 'Koenigsegg', '007');
+    document.write(car1.toString());
+    const car2 = new ServiceCar('Emilien', 'Trabant', '000');
+    document.write(`Створюємо, та змінюємо на: <br> Водій - Emilien <br>
+	Марка авто - Trabant <br> Номер авто - 000 <br> <br>Результат: <br> <br> ${car2}`);
+}
---
+++ lesson_17/src/task_1.ts
@@ -0,0 +1,36 @@
+if (confirm('Почати тестування?')) {
+	class Test {
+		static sumAdditional = 0
+		static sumNegatives = 0
+		static sumNumberTwo = 0
+		numbersList: number[]
+		constructor(numbersList: number[]) {
+			this.numbersList = numbersList
+		}
+		getSumAdditional() {
+			Test.sumAdditional = this.numbersList.reduce((prevSum, el) => el > 0 ? prevSum + 1 : prevSum, 0)
+		}
+		getSumNegatives() {
+			Test.sumNegatives = this.numbersList.reduce((prevSum, el) => el < 0 ? prevSum + 1 : prevSum, 0)
+		}
+		getSumNumberTwo() {
+			Test.sumNumberTwo = this.numbersList.reduce((prevSum, el) => el === 2 ? prevSum + 1 : prevSum, 0)
+		}
+		toString() {
+			return `Кількість додатних - ${Test.sumAdditional}<br>
+				Кількість від’ємних - ${Test.sumNegatives} <br>
+				Кількість входжень числа "2" - ${Test.sumNumberTwo}<br>`
+		}
+		run() {
+			this.getSumAdditional()
+			this.getSumNegatives()
+			this.getSumNumberTwo()
+		}
+	}
+
+
+	const test1 = new Test([2, 5, 10, -13, 4, 2, -20, -3, 8, 95, 45, 2, -17, 78, -84, 2])
+	test1.run()
+	document.write(test1.toString())
+}
+
