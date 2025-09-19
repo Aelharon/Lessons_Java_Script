fb45609730e794f5e3b48e054a41b8df9e7c5178
Aelharon <Aelharon@gmail.com>
2025-09-19 16:03:16 +0300

    Update lesson 17

--- lesson_17/js/task_2.js
+++ lesson_17/js/task_2.js
@@ -1,26 +1,55 @@
 "use strict";
+// Варіант 1 не відповідає умові
+// if (confirm('Почати тестування?')) {
+// 	class ServiceCar {
+// 		static driverRef: string
+// 		static brandRef: string
+// 		static autoNumberRef: string
+// 		constructor(driver: string, brand: string, autoNumber: string) {
+// 			if (ServiceCar.driverRef) return ServiceCar.driverRef
+// 			if (ServiceCar.brandRef) return ServiceCar.brandRef
+// 			if (ServiceCar.autoNumberRef) return ServiceCar.autoNumberRef
+// 			ServiceCar.driverRef = driver
+// 			ServiceCar.brandRef = brand
+// 			ServiceCar.autoNumberRef = autoNumber
+// 		}
+// 		toString() {
+// 			return `Водій - ${ServiceCar.driverRef} <br>
+// 				Марка авто - ${ServiceCar.brandRef} <br>
+// 				Номер авто - ${ServiceCar.autoNumberRef}<hr>`
+// 		}
+// 	}
+// 	const car1 = new ServiceCar('Ken Block', 'Koenigsegg', '007')
+// 	document.write(car1.toString())
+// 	const car2 = new ServiceCar('Emilien', 'Trabant', '000')
+// 	document.write(`Створюємо, та змінюємо на: <br> Водій - Emilien <br>
+// 	Марка авто - Trabant <br> Номер авто - 000 <br> <br>Результат: <br> <br> ${car2}`)
+// }
+// Варіант 2--------------------------------------------------------------------------------------
 if (confirm('Почати тестування?')) {
     class ServiceCar {
         constructor(driver, brand, autoNumber) {
-            if (ServiceCar.driverRef)
-                return ServiceCar.driverRef;
-            if (ServiceCar.brandRef)
-                return ServiceCar.brandRef;
-            if (ServiceCar.autoNumberRef)
-                return ServiceCar.autoNumberRef;
-            ServiceCar.driverRef = driver;
-            ServiceCar.brandRef = brand;
-            ServiceCar.autoNumberRef = autoNumber;
+            if (ServiceCar.serviceCarRef)
+                return ServiceCar.serviceCarRef;
+            this.driver = driver;
+            this.brand = brand;
+            this.autoNumber = autoNumber;
+            this.serviceCar = this.getArrayServiceCar();
+            ServiceCar.serviceCarRef = this;
+        }
+        getArrayServiceCar() {
+            const arr = [this.driver, this.brand, this.autoNumber];
+            return arr;
         }
         toString() {
-            return `Водій - ${ServiceCar.driverRef} <br>
-				Марка авто - ${ServiceCar.brandRef} <br>
-				Номер авто - ${ServiceCar.autoNumberRef}<hr>`;
+            return `Водій - ${this.serviceCar[0]} <br>
+				Марка авто - ${this.serviceCar[1]} <br>
+				Номер авто - ${this.serviceCar[2]}<hr>`;
         }
     }
     const car1 = new ServiceCar('Ken Block', 'Koenigsegg', '007');
-    document.write(car1.toString());
     const car2 = new ServiceCar('Emilien', 'Trabant', '000');
+    document.write(car1.toString());
     document.write(`Створюємо, та змінюємо на: <br> Водій - Emilien <br>
-	Марка авто - Trabant <br> Номер авто - 000 <br> <br>Результат: <br> <br> ${car2}`);
+Марка авто - Trabant <br> Номер авто - 000 <br> <br>Результат: <br> <br> ${car2}`);
 }
--- lesson_17/src/task_1.ts
+++ lesson_17/src/task_1.ts
@@ -1,36 +1,56 @@
+// 1 Варіант не вірно за умовою
+// if (confirm('Почати тестування?')) {
+// class Test {
+// 	static sumAdditional = 0
+// 	static sumNegatives = 0
+// 	static sumNumberTwo = 0
+// 	numbersList: number[]
+// 	constructor(numbersList: number[]) {
+// 		this.numbersList = numbersList
+// 	}
+// 	getSumAdditional() {
+// 		Test.sumAdditional = this.numbersList.reduce((prevSum, el) => el > 0 ? prevSum + 1 : prevSum, 0)
+// 	}
+// 	getSumNegatives() {
+// 		Test.sumNegatives = this.numbersList.reduce((prevSum, el) => el < 0 ? prevSum + 1 : prevSum, 0)
+// 	}
+// 	getSumNumberTwo() {
+// 		Test.sumNumberTwo = this.numbersList.reduce((prevSum, el) => el === 2 ? prevSum + 1 : prevSum, 0)
+// 	}
+// 	toString() {
+// 		return `Кількість додатних - ${Test.sumAdditional}<br>
+// 			Кількість від’ємних - ${Test.sumNegatives} <br>
+// 			Кількість входжень числа "2" - ${Test.sumNumberTwo}<br>`
+// 	}
+// 	run() {
+// 		this.getSumAdditional()
+// 		this.getSumNegatives()
+// 		this.getSumNumberTwo()
+// 	}
+// }
+// const test1 = new Test([2, 5, 10, -13, 4, 2, -20, -3, 8, 95, 45, 2, -17, 78, -84, 2])
+// test1.run()
+// document.write(test1.toString())
+// }
+//-------------------------------------------------------------------------------------
+// 2 Варіант 
 if (confirm('Почати тестування?')) {
 	class Test {
 		static sumAdditional = 0
 		static sumNegatives = 0
 		static sumNumberTwo = 0
-		numbersList: number[]
-		constructor(numbersList: number[]) {
-			this.numbersList = numbersList
+
+		static getSumAdditional(numbersList: number[]) {
+			return this.sumAdditional = numbersList.reduce((prevSum, el) => el > 0 ? prevSum + 1 : prevSum, 0)
 		}
-		getSumAdditional() {
-			Test.sumAdditional = this.numbersList.reduce((prevSum, el) => el > 0 ? prevSum + 1 : prevSum, 0)
+		static getSumNegatives(numbersList: number[]) {
+			return this.sumNegatives = numbersList.reduce((prevSum, el) => el < 0 ? prevSum + 1 : prevSum, 0)
 		}
-		getSumNegatives() {
-			Test.sumNegatives = this.numbersList.reduce((prevSum, el) => el < 0 ? prevSum + 1 : prevSum, 0)
-		}
-		getSumNumberTwo() {
-			Test.sumNumberTwo = this.numbersList.reduce((prevSum, el) => el === 2 ? prevSum + 1 : prevSum, 0)
-		}
-		toString() {
-			return `Кількість додатних - ${Test.sumAdditional}<br>
-				Кількість від’ємних - ${Test.sumNegatives} <br>
-				Кількість входжень числа "2" - ${Test.sumNumberTwo}<br>`
-		}
-		run() {
-			this.getSumAdditional()
-			this.getSumNegatives()
-			this.getSumNumberTwo()
+		static getSumNumberTwo(numbersList: number[]) {
+			return this.sumNumberTwo = numbersList.reduce((prevSum, el) => el === 2 ? prevSum + 1 : prevSum, 0)
 		}
 	}
-
-
-	const test1 = new Test([2, 5, 10, -13, 4, 2, -20, -3, 8, 95, 45, 2, -17, 78, -84, 2])
-	test1.run()
-	document.write(test1.toString())
+	document.write(`Кількість додатних - ${Test.getSumAdditional([2, 5, 10, -13, 4, 2, -20, -3, 8, 95, 45, 2, -17, 78, -84, 2])} <br>`)
+	document.write(`Кількість від’ємних - ${Test.getSumNegatives([2, 5, 10, -13, 4, 2, -20, -3, 8, 95, 45, 2, -17, 78, -84, 2])} <br>`)
+	document.write(`Кількість входжень числа "2" - ${Test.getSumNumberTwo([2, 5, 10, -13, 4, 2, -20, -3, 8, 95, 45, 2, -17, 78, -84, 2])} <br>`)
 }
-
